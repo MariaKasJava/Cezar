72d402470d9da895703a4778ff920f85d193793d
Mariia Kasianchuk <mariabalkas25@gmail.com>
2022-12-11 11:37:21 +0100

    new BruteForce

---
+++ src/Brut.java
@@ -0,0 +1,21 @@
+public class Brut {
+    public static StringBuilder decoder1(String b, int count) {
+
+        return Decoder.getStringBuilder(b, count);
+    }
+
+    public static void decoder2() {
+        for (int i = 0; i <= Cezar111.alfavit.length(); i++) {
+            System.out.println(decoder1(Cezar111.line, i));
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
