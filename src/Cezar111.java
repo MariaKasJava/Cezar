8c7c954b0429abf98dd2d5e616a43c1e0f306238
Mariia Kasianchuk <mariabalkas25@gmail.com>
2022-12-13 17:27:20 +0100

    writerider add

--- src/Cezar111.java
+++ src/Cezar111.java
@@ -1,19 +1,20 @@
+import javax.swing.*;
 import java.io.*;
 import java.util.Scanner;
 
 public class Cezar111 {
     public static final String alfavit = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ,-.? :!";
 
-    public static String getLine() {
-        return line;
-    }
+
 
     public static String line;
-    public static String getAlfavit() {
-        return alfavit;
-    }
+    public static int key;
+
 
     public static void main(String[] args) throws IOException {
+
+
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введте имя файла");
         String fileName = scanner.nextLine();
@@ -25,10 +26,11 @@ public class Cezar111 {
         String otvet = scanner.nextLine();
         if (otvet.equalsIgnoreCase("шифровать")) {
             System.out.println("Введте ключ щифра");
-            int key = scanner.nextInt();
+            key = scanner.nextInt();
             while ((line = reader.readLine()) != null) {
                 stringBuilder.append(line);
                 stringBuilder = Contracenia.contrasenia(line, key);
+
             }
         } else if (otvet.equalsIgnoreCase("разшифровать")) {
             System.out.println("Введте ключ дещифровки");
@@ -36,24 +38,25 @@ public class Cezar111 {
             while ((line = reader.readLine()) != null) {
                 stringBuilder.append(line);
                 stringBuilder = Decoder.decoder(line, decod);
+
             }
 
         } else if (otvet.equalsIgnoreCase("brute force")) {
 
             while ((line = reader.readLine()) != null) {
                 stringBuilder.append(line);
-             BruteForce.decoder2();
 
+                BruteForce.decoder2();
 
             }
+           reader.close();
 
-       reader.close();
 
-        BufferedWriter writer = new BufferedWriter(new FileWriter(new File("text1")));
+
+    }
+        BufferedWriter writer = new BufferedWriter(new FileWriter(new File("text2")));
         writer.write(stringBuilder.toString());
         writer.close();
-    }
-
 
 }
 
